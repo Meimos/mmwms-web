f7b5438e71bcbafeb795efdd8eb8b2cebf723a8a
liangchao03290014 <1249744124@qq.com>
2020-06-12 15:23:45 +0800

    提交public-api下的config\job\notice的相关代码

--- src/app/shared/models/public-api.ts
+++ src/app/shared/models/public-api.ts
@@ -4,3 +4,6 @@ export * from './page.model';
 export * from './role.model';
 export * from './menu.model';
 export * from './base.model';
+export * from './config.model';
+export * from './notice.model';
+export * from './job.model' ;
