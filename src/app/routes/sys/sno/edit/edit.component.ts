ca1789a5598eb5e6283b2fd35456fbbe9bf17e06
Meimosor <vondser@gmail.com>
2020-06-13 21:46:46 +0800

    新增：流水号配置功能

--- src/app/routes/sys/sno/edit/edit.component.ts
+++ src/app/routes/sys/sno/edit/edit.component.ts
@@ -1,25 +1,44 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { SFSchema, SFUISchema } from '@delon/form';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { SFComponent, SFSchema, SFSelectWidgetSchema, SFUISchema } from '@delon/form';
 import { _HttpClient } from '@delon/theme';
+import { dictMap, DictType, ISysSno, SysSno } from '@shared';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { NzModalRef } from 'ng-zorro-antd/modal';
-
 @Component({
   selector: 'app-sys-sno-edit',
   templateUrl: './edit.component.html',
 })
 export class SysSnoEditComponent implements OnInit {
-  record: any = {};
-  i: any;
+  @Input()
+  record: ISysSno;
+
+  formData: any;
+
+  @ViewChild('sf', { static: false }) private sf: SFComponent;
   schema: SFSchema = {
     properties: {
-      no: { type: 'string', title: '编号' },
-      owner: { type: 'string', title: '姓名', maxLength: 15 },
-      callNo: { type: 'number', title: '调用次数' },
-      href: { type: 'string', title: '链接', format: 'uri' },
-      description: { type: 'string', title: '描述', maxLength: 140 },
+      name: { type: 'string', title: '流水号名称', minLength: 5 },
+      alias: { type: 'string', title: '别名', minLength: 6 },
+      regulation: { type: 'string', title: '规则', maxLength: 30 },
+      noLength: { type: 'number', title: '流水号长度', maxLength: 20 },
+      initValue: { type: 'number', title: '初始值', maxLength: 20, default: 0 },
+      step: { type: 'number', title: '步长', maxLength: 20, default: 0 },
+      genType: {
+        type: 'number',
+        title: '生成类型',
+        enum: [
+          { label: dictMap.get(DictType.SNO_GENTYPE)[0].label, value: Number(dictMap.get(DictType.SNO_GENTYPE)[0].value) },
+          { label: dictMap.get(DictType.SNO_GENTYPE)[1].label, value: Number(dictMap.get(DictType.SNO_GENTYPE)[1].value) },
+          { label: dictMap.get(DictType.SNO_GENTYPE)[2].label, value: Number(dictMap.get(DictType.SNO_GENTYPE)[2].value) },
+          { label: dictMap.get(DictType.SNO_GENTYPE)[3].label, value: Number(dictMap.get(DictType.SNO_GENTYPE)[3].value) }
+        ],
+        ui: {
+          widget: 'select',
+        } as SFSelectWidgetSchema,
+        default: Number(dictMap.get(DictType.SNO_GENTYPE)[0].value),
+      }
     },
-    required: ['owner', 'callNo', 'href', 'description'],
+    required: ['name', 'alias', 'regulation', 'noLength', 'genType'],
   };
   ui: SFUISchema = {
     '*': {
@@ -29,12 +48,8 @@ export class SysSnoEditComponent implements OnInit {
     $no: {
       widget: 'text',
     },
-    $href: {
-      widget: 'string',
-    },
-    $description: {
-      widget: 'textarea',
-      grid: { span: 24 },
+    $genType: {
+      grid: { span: 12 },
     },
   };
 
@@ -42,15 +57,30 @@ export class SysSnoEditComponent implements OnInit {
 
   ngOnInit(): void {
     if (this.record.id > 0) {
-      this.http.get(`/user/${this.record.id}`).subscribe((res) => (this.i = res));
+      this.http.get('system/sno/' + this.record.id).subscribe((res) => {
+        this.formData = res.sn;
+        console.log(res);
+        
+      });
+    } else {
+      this.formData = new SysSno(null);
     }
   }
 
-  save(value: any) {
-    this.http.post(`/user/${this.record.id}`, value).subscribe((res) => {
-      this.msgSrv.success('保存成功');
-      this.modal.close(true);
-    });
+  saveOrUpdate(sysSno: SysSno) {
+    if (sysSno.id > 0) {
+      this.http.put('system/sno', sysSno).subscribe((res) => {
+        if (res.code === 200) {
+          this.modal.close(true);
+        }
+      });
+    } else {
+      this.http.post('system/sno', sysSno).subscribe((res) => {
+        if (res.code === 200) {
+          this.modal.close(true);
+        }
+      });
+    }
   }
 
   close() {
